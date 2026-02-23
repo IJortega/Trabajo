477fbe725a66f7d046492a503561cadaf3ce3835
IJortega <isaiasdejesusortegavargas@gmail.com>
2026-02-22 20:08:36 -0500

    datos personales listo

---
+++ Tarea.py
@@ -0,0 +1,18 @@
+print("Ingrese su primer nombre:")
+nom = input()
+
+print("Ingrese su segundo nombre:")
+nom_2 = input()
+
+print("Ingrese su primer apellido:")
+ape = input()
+
+print("Ingrese su segundo apellido:")
+ape_2 = input()
+
+print("Ingrese su edad:")
+edad = input()
+
+print("Ingrese su año de nacimiento:")
+año_nac = input()
+
