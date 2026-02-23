8a32ed1165276e7904b64d8524525ca886800787
IJortega <isaiasdejesusortegavargas@gmail.com>
2026-02-22 22:17:00 -0500

    Datos del usuario listos

---
+++ Tarea.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+void main() {
+    print("Ingrese su primer nombre:");
+    String nom = stdin.readLineSync()!;
+
+    print("Ingrese su segundo nombre:");
+    String nom_2 = stdin.readLineSync()!;
+
+    print("Ingrese su primer apellido:");
+    String ape = stdin.readLineSync()!;
+
+    print("Ingrese su segundo apellido:");
+    String ape_2 = stdin.readLineSync()!;
+
+    print("Ingrese su edad:");
+    String edad = stdin.readLineSync()!;
+
+    print("Ingrse su año de nacimiento:");
+    String anioNac = stdin.readLineSync()!;
+
+    
+
+    
+}
+
